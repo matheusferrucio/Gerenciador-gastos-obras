8968abd2f7a7b0ca5cf68d7f58d7f2efe7db4294
Matheus <matheusferrucio29@gmail.com>
2026-02-23 17:24:06 -0300

    Alterei a query que busca e calcula os valores dos cards de resumo do dashboard, agora a query já retorna o valor das comissões calculando dinamicamente a soma das comissões de cada obra calculadas com base na porcentagem de cobrança definida no cadastro

--- pages/back/views/view_resumodb.php
+++ pages/back/views/view_resumodb.php
@@ -5,10 +5,10 @@
       // Estou utilizando LEFT JOIN para que todas as obras entrem na contagem independentemente
       // se possuem gastos registrados ou não
       $query = $conn->prepare("SELECT
-                                 SUM(g.valor_gasto) * 0.1 AS total_comissao_obras,
+                                 SUM(g.valor_gasto * (o.porcentagem_cobranca / 100)) AS total_comissao_obras,
                                  SUM(g.valor_gasto) AS total_gastos_obras,
                                  COUNT(DISTINCT o.id) AS qtd_total_obras,
-                                 SUM(g.valor_gasto) / COUNT(DISTINCT o.id) AS ticket_medio_obra
+                                 SUM(g.valor_gasto * (o.porcentagem_cobranca / 100)) / COUNT(DISTINCT o.id) AS ticket_medio_obra
                                  FROM obras o
                                  LEFT JOIN gastosobras g
                                  ON o.id = g.id_obra");

f938a3178f6ae3aaedfe375e7878348e38f74705
Matheus <matheusferrucio29@gmail.com>
2026-02-25 15:26:55 -0300

    Criei uma página simples para tratar o catch quando tentamos excluir um obra que contém gastos cadastrados atrelados a ela

--- pages/back/excluir/excluir_obradb.php
+++ pages/back/excluir/excluir_obradb.php
@@ -20,7 +20,12 @@
       );
 
    } catch(PDOException $erro) {
-      echo "Não foi possível excluir a obra";
+      $msgErro = 'Não foi possível excluir a obra, há 1 ou mais gastos atrelados a ela';
+      $title = 'Exclusão não permitida';
+      $caminhoVolta = BASE_URL . "pages/front/listas/lista_obras.php";
+      
+      // echo "Não foi possível excluir a obra";
+      header("Location:" . BASE_URL . "pages/front/error.php?erro=$msgErro&title=$title&caminho_volta=$caminhoVolta");
       exit();
    }
 ?>
--- pages/front/error.php
+++ pages/front/error.php
@@ -1,1 +1,30 @@
-<!-- Página padrão para exibição de mensagens de erro -->
+<?php
+   require_once __DIR__."/../back/config.php";
+
+   $title = isset($_GET['title']) ? $_GET['title'] : 'testando';
+
+   $erro = isset($_GET['erro']) ? $_GET['erro'] : 'testando';
+
+   $caminhoVolta = isset($_GET['caminho_volta']) ? $_GET['caminho_volta'] : '#';
+?>
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="<?= BASE_URL; ?>css/reset.css">
+      <link rel="stylesheet" href="<?= BASE_URL; ?>css/style.css">
+      <link rel="stylesheet" href="<?= BASE_URL; ?>css/home.css">
+      <link rel="stylesheet" href="<?= BASE_URL; ?>css/error.css">
+      <title><?= $title; ?></title>
+   </head>
+   <body>
+      <div class="container">
+         <div class="card">
+            <h1><?= $erro ?></h1>
+            <a href="<?= $caminhoVolta; ?>" class="btn">Voltar</a>
+         </div>
+      </div>
+   </body>
+</html>

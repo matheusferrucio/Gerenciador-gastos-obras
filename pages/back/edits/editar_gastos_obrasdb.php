306651d1be390c12d29b1378ab5176557f10e4fd
Matheus <matheusferrucio29@gmail.com>
2026-04-02 11:56:28 -0300

    Alterei o arquivo de editar_gasto_obra de  modo que ele recupere o valor pelo input hidden, e não o input que aparece para o usuário

--- pages/back/edits/editar_gastos_obrasdb.php
+++ pages/back/edits/editar_gastos_obrasdb.php
@@ -6,7 +6,7 @@
 
       $idGastoObra    = $_POST['id'];
       $idObra         = $_POST['obraGasto'];
-      $valorGastoObra = filter_input(INPUT_POST, 'gastoObra', FILTER_SANITIZE_SPECIAL_CHARS);
+      $valorGastoObra = filter_input(INPUT_POST, 'valor', FILTER_VALIDATE_FLOAT);
       $dataGasto      = $_POST['dataGasto'];
       $descricao      = filter_input(INPUT_POST, 'descricaoGasto', FILTER_SANITIZE_SPECIAL_CHARS);
 

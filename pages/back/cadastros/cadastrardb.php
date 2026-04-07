3a3ea8927e9c4c8b216fe43810e59530aa80d3b2
Matheus <matheusferrucio29@gmail.com>
2026-04-07 16:25:44 -0300

    Adicionei camadas de verificação para segurança no arquivo que valida o cadastro do usuário

--- pages/back/cadastros/cadastrardb.php
+++ pages/back/cadastros/cadastrardb.php
@@ -1,13 +1,22 @@
 <?php
+   if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+      throw new Exception("Método de requisição inválido");
+   }
+
    require_once(__DIR__."/../../conexao/connection.php");
 
    require_once(__DIR__."/../config.php");
 
-   $nomeUsuario  = filter_input(INPUT_POST, 'nomeUsuario', FILTER_SANITIZE_SPECIAL_CHARS);
-   $cpfUsuario = filter_input(INPUT_POST, 'cpfUsuario', FILTER_SANITIZE_SPECIAL_CHARS);
-   $senhaUsuario = trim($_POST['senhaUsuario']);
-
    try {
+
+      $nomeUsuario  = filter_input(INPUT_POST, 'nomeUsuario', FILTER_SANITIZE_SPECIAL_CHARS);
+      $cpfUsuario = filter_input(INPUT_POST, 'cpfUsuario', FILTER_SANITIZE_SPECIAL_CHARS);
+      $senhaUsuario = trim($_POST['senhaUsuario']);
+
+      if (empty($nomeUsuario) || !filter_var($nomeUsuario, FILTER_VALIDATE_STRING) || empty($cpfUsuario) || !filter_var($cpfUsuario, FILTER_VALIDATE_INT) || empty($senhaUsuario)) {
+         throw new Exception("Preencha todos os campos para cadastrar usuário");
+      }
+      
       $query = $conn->prepare("INSERT INTO usuarios (
                                  cpf,
                                  nome,
@@ -24,18 +33,20 @@
          ":senha" => password_hash($senhaUsuario, PASSWORD_DEFAULT)
       ]);
 
-      if ($query) {
-         session_start();
-
-         $_SESSION['usuario'] = $cpfUsuario;
-         $_SESSION['nomeUsuario'] = $nomeUsuario;
-         
-         header("location:".BASE_URL."pages/front/home.php");
-         exit();
+      if (!$query) {
+         throw new Exception("Não foi possível cadastrar usuário");
       }
 
-   } catch (PDOException $erro) {
-      echo "Não foi possível cadastrar usuário";
+      session_start();
+
+      $_SESSION['usuario'] = $cpfUsuario;
+      $_SESSION['nomeUsuario'] = $nomeUsuario;
+      
+      header("location:".BASE_URL."pages/front/home.php");
       exit();
+
+   } catch (PDOException $erro) {
+      echo $erro->getMessage();
+      die();
    }
 ?>
